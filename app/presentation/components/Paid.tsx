1eba805f879ab39da3676dadd4e975ff52f3b9bb
Felipe José <lipejose@icloud.com>
2022-04-02 18:58:04 -0300

    fix: typo

--- app/presentation/components/Paid.tsx
+++ app/presentation/components/Paid.tsx
@@ -5,7 +5,7 @@ type Paid = {
 export default function Paid({ orderId }: Paid) {
   return (
     <div className='items-center space-y-2'>
-      <h1 className='text-2xl'>Pago com successo</h1>
+      <h1 className='text-2xl'>Pago com sucesso</h1>
       <h1 className='text-xl'>orderId: {orderId}</h1>
     </div>
   );
