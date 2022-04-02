1cee8db7927f303a26603f43ad95f300e2a29222
Felipe José <lipejose@icloud.com>
2022-04-02 15:01:50 -0300

    refactor: atore and checkout page

--- app/routes/checkout/index.tsx
+++ app/routes/checkout/index.tsx
@@ -1,6 +1,6 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useForm } from "react-hook-form";
-import {v4 as uuid } from "uuid"
+import { v4 as uuid } from "uuid";
 
 import Card from "~/presentation/components/Card";
 import Cart from "~/presentation/components/Cart";
@@ -11,65 +11,153 @@ import Paying from "~/presentation/components/Paid";
 import PaymentInProgress from "~/presentation/components/PaymentInProgress";
 
 import cardBrand from "~/utils/card-brand";
+import { getInitialValues } from "~/utils/get-initial-values";
+import { PaymentService } from "../../domain/service/paymentService";
+import { calculateCartTotal } from "~/utils/calculate-card-total";
 
-const inputStyle = "input input-bordered input-sm w-full";
+import type { Cart as CartType } from "~/domain/types/cart";
+import { checkFields } from "~/utils/check-fields";
+import { formatValidate } from "~/utils/format-date";
+import { Payment } from "~/domain/types/payment";
 
 export default function CheckoutPage() {
   const orderId = uuid();
+  const [cart, setCart] = useState(getInitialValues() as CartType);
   const { register, handleSubmit, watch } = useForm();
-  const [data, setData] = useState({});
   const [waiting, setWaiting] = useState(false);
   const [payout, setPayout] = useState(false);
   const [checkout, setCheckout] = useState(true);
-  const [error, setError] = useState(true);
-
+  const [error, setError] = useState("");
   const brand = cardBrand(watch("cardNumber"));
+  const canShowCart = cart?.length > 0;
+  const fieldsErrorMessage = "Please enter only numbers in card fields";
+
+  const handleClear = () => {
+    setCart([]);
+  };
+
+  const validateFields = ({ cardNumber, cardValidate, cardCCV }: Payment) => {
+    if (
+      checkFields(cardNumber) ||
+      checkFields(cardValidate) ||
+      checkFields(cardCCV)
+    )
+      return true;
+
+    return false;
+  };
+
+  if (!canShowCart) {
+    if (typeof window !== "undefined") window.location.pathname = "/store";
+  }
+
+  const handleError = (message: string) => {
+    setError(message);
+    setWaiting(false);
+    setCheckout(true);
+  };
+
+  const handleSuccess = () => {
+    setWaiting(false);
+    setPayout(true);
+  };
+
+  const handlePayment = async (
+    data: Payment,
+    brand: string,
+    amount: number
+  ) => {
+    if (validateFields(data)) {
+      setError(fieldsErrorMessage);
+      return;
+    }
+
+    const [firstName, lastName] = data.name.split(" ");
+    const validate = formatValidate(data.cardValidate);
+    const service = new PaymentService({
+      card: {
+        brand,
+        ccv: data.cardCCV,
+        exp: validate,
+        name: data.name,
+        number: data.cardNumber,
+      },
+      customer: { firstName, lastName },
+      order: { amount, orderid: orderId },
+      provider: "cielo",
+    });
+
+    setCheckout(false);
+    setWaiting(true);
+
+    try {
+      const response = await service.execute();
+
+      if (response.getCode() !== 200) {
+        handleError(response.getMessage());
+        return;
+      }
+
+      handleSuccess();
+    } catch (err) {
+      handleError((err as Error).message);
+    }
+  };
+
+  useEffect(() => {
+    localStorage.setItem("cart", JSON.stringify(cart));
+  }, [cart]);
 
   return (
     <Content>
-      <Cart />
+      {canShowCart && <Cart cart={cart} clear={() => handleClear()} />}
       {!!waiting && <PaymentInProgress />}
-      {!!payout && <Paying orderId='test' />}
-      {!!error && <Error message='teste' close={() => setError(!error)} />}
+      {!!payout && <Paying orderId={orderId} />}
+      {!!error && <Error message={error} close={() => setError("")} />}
       {!!checkout && (
-        <form onSubmit={handleSubmit((data) => setData(data))}>
+        <form
+          onSubmit={handleSubmit((data) =>
+            handlePayment(
+              data as Payment,
+              brand.brand,
+              calculateCartTotal(cart)
+            )
+          )}
+        >
           <CollapseContent title='Dados Pessoais'>
             <input
               {...register("name")}
               placeholder='Nome Completo'
-              className={inputStyle}
+              className='input input-bordered input-sm w-full'
             />
             <input
               {...register("email")}
               type='email'
               placeholder='E-mail'
-              className={inputStyle}
+              className='input input-bordered input-sm w-full'
             />
           </CollapseContent>
           <CollapseContent title='Dados do cartão'>
             <Card {...watch()} />
             <div className='flex flex-col pt-4 space-y-2 text-white'>
               <input
-                {...register("cardNumber")}
                 placeholder='Número Cartão'
                 maxLength={16}
-                type='number'
-                className={inputStyle}
+                {...register("cardNumber")}
+                className='input input-bordered input-sm w-full '
               />
               <div className='flex justify-between space-x-2'>
                 <input
                   {...register("cardValidate")}
                   placeholder='Validade'
                   maxLength={6}
-                  type='text'
-                  className={inputStyle}
+                  className='input input-bordered input-sm w-full'
                 />
                 <input
                   {...register("cardCCV")}
                   placeholder='CCV'
                   maxLength={3}
-                  type='text'
-                  className={inputStyle}
+                  className='input input-bordered input-sm w-full'
                 />
               </div>
             </div>
--- app/routes/store/index.tsx
+++ app/routes/store/index.tsx
@@ -1,13 +1,18 @@
 import Cart from "~/presentation/components/Cart";
 import Product from "~/presentation/components/Product";
-import adrenalineBoosters from "~/presentation/assets/adrenaline-booster.png"
-import bioConductor from "~/presentation/assets/bioconductor.png"
-import bioPlasticBloodVessel from "~/presentation/assets/bioplastic-blood-vessel.png"
-import feedbackCircuit from "~/presentation/assets/feedbackcircuit.png"
-import secondHeart from "~/presentation/assets/second-heart.png"
-import tyrosineInjector from "~/presentation/assets/tyrosine-injector.png"
+import adrenalineBoosters from "~/presentation/assets/adrenaline-booster.png";
+import bioConductor from "~/presentation/assets/bioconductor.png";
+import bioPlasticBloodVessel from "~/presentation/assets/bioplastic-blood-vessel.png";
+import feedbackCircuit from "~/presentation/assets/feedbackcircuit.png";
+import secondHeart from "~/presentation/assets/second-heart.png";
+import tyrosineInjector from "~/presentation/assets/tyrosine-injector.png";
 import Content from "~/presentation/components/Content";
+import { useEffect, useState } from "react";
+import { getInitialValues } from "~/utils/get-initial-values";
 
+import type { Cart as CartType } from "~/domain/types/cart";
+import type { Product as ProductType } from "~/domain/types/product";
+import type { CartItem } from "~/domain/types/cart-item";
 
 export const data = [
   {
@@ -17,7 +22,7 @@ export const data = [
     img: adrenalineBoosters,
   },
   {
-    name: 'Bio-Conductor',
+    name: "Bio-Conductor",
     id: "2",
     price: 1394907,
     img: bioConductor,
@@ -36,25 +41,53 @@ export const data = [
   },
   {
     name: "Second Heart",
-    id: "2",
+    id: "5",
     price: 37905,
     img: secondHeart,
   },
   {
     name: "Tyrosine Injector",
-    id: "2",
+    id: "6",
     price: 29990,
     img: tyrosineInjector,
   },
 ];
 
 export default function StorePage() {
+  const [cart, setCart] = useState(getInitialValues() as CartType);
+
+  const addProd = (prod: ProductType) => {
+    if (getProductFromCart(prod.id)) {
+      setCart(
+        cart.map((product) =>
+          product.id === prod.id ? addQtd(product) : product
+        )
+      );
+    } else {
+      setCart([...cart, { ...prod, qtd: 1 }]);
+    }
+  };
+
+  const getProductFromCart = (id: string) =>
+    cart.find((cartProd) => cartProd.id === id);
+
+  const addQtd = (product: CartItem) => ({
+    ...product,
+    qtd: product.qtd + 1,
+  });
+
+  useEffect(() => {
+    localStorage.setItem("cart", JSON.stringify(cart));
+  }, [cart]);
+
   return (
     <Content>
-      <Cart />
+      <Cart cart={cart} clear={() => setCart([])} />
       <div className='grid grid-cols-3'>
         {data.map((prod) => {
-          return <Product {...prod} />;
+          return (
+            <Product key={prod.id} prod={prod} add={() => addProd(prod)} />
+          );
         })}
       </div>
     </Content>
