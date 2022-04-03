d658331661ba4049c2b3280a7d17c8c27c32e5e4
Felipe José <lipejose@icloud.com>
2022-04-03 16:02:47 -0300

    refactor: use fetcher

--- app/routes/checkout.tsx
+++ app/routes/checkout.tsx
@@ -1,3 +1,4 @@
+import { ActionFunction, useFetcher } from "remix";
 import { useEffect, useState } from "react";
 import { useForm } from "react-hook-form";
 import { v4 as uuid } from "uuid";
@@ -6,34 +7,89 @@ import Card from "~/presentation/components/Card";
 import Cart from "~/presentation/components/Cart";
 import CollapseContent from "~/presentation/components/CollapseContent";
 import Content from "~/presentation/components/Content";
-import Error from "~/presentation/components/Error";
+import ToastError from "~/presentation/components/ToastError";
 import Paying from "~/presentation/components/Paid";
 import PaymentInProgress from "~/presentation/components/PaymentInProgress";
 
-import cardBrand from "~/utils/card-brand";
+import { cardBrand } from "~/utils/card-brand";
 import { getInitialValues } from "~/utils/get-initial-values";
-import { PaymentService } from "../../domain/service/paymentService";
+import { PaymentService } from "../domain/service/paymentService";
 import { calculateCartTotal } from "~/utils/calculate-card-total";
-
-import type { Cart as CartType } from "~/domain/types/cart";
 import { checkFields } from "~/utils/check-fields";
 import { formatValidate } from "~/utils/format-date";
-import { Payment } from "~/domain/types/payment";
+
+import type { Payment } from "~/domain/types/payment";
+import type { Cart as CartType } from "~/domain/types/cart";
+import { DEFAULT_PROVIDER } from "~/config/env";
+
+export const action: ActionFunction = async ({ request }) => {
+  const formData = await request.formData();
+  const orderId = uuid();
+  const name = formData.get("name") as string;
+  const cardNumber = formData.get("cardNumber") as string;
+  const cardValidate = formData.get("cardValidate") as string;
+  const cardCCV = formData.get("cardCCV") as string;
+  const brand = formData.get("brand") as string;
+  const amount = calculateCartTotal(JSON.parse(formData.get("cart") as string));
+
+  const [firstName, lastName] = name.split(" ");
+  const validate = formatValidate(cardValidate);
+  const service = new PaymentService({
+    card: {
+      brand,
+      ccv: cardCCV,
+      exp: validate,
+      name: name,
+      number: cardNumber,
+    },
+    customer: { firstName, lastName },
+    order: { amount, orderid: orderId },
+    provider: DEFAULT_PROVIDER,
+  });
+
+  const response = await service.execute();
+
+  return { code: response.getCode(), message: response.getMessage(), orderId };
+};
 
 export default function CheckoutPage() {
-  const orderId = uuid();
+  const fetcher = useFetcher();
   const [cart, setCart] = useState(getInitialValues() as CartType);
   const { register, handleSubmit, watch } = useForm();
   const [waiting, setWaiting] = useState(false);
   const [payout, setPayout] = useState(false);
   const [checkout, setCheckout] = useState(true);
+  const [order, setOrder] = useState("");
   const [error, setError] = useState("");
   const brand = cardBrand(watch("cardNumber"));
   const canShowCart = cart?.length > 0;
-  const fieldsErrorMessage = "Please enter only numbers in card fields";
+  const response = fetcher.data;
+  const fieldsErrorMessage =
+    "Por favor digite apenas números nos campos do cartão";
 
   const handleClear = () => {
     setCart([]);
+  };
+
+  if (!canShowCart) {
+    if (typeof window !== "undefined") window.location.pathname = "/store";
+  }
+
+  const handleError = (message: string) => {
+    setError(message);
+    setWaiting(false);
+    setCheckout(true);
+  };
+
+  const handleSuccess = (orderId: string) => {
+    setOrder(orderId);
+    setWaiting(false);
+    setPayout(true);
+  };
+
+  const handleRequest = () => {
+    setWaiting(true);
+    setCheckout(false);
   };
 
   const validateFields = ({ cardNumber, cardValidate, cardCCV }: Payment) => {
@@ -47,80 +103,41 @@ export default function CheckoutPage() {
     return false;
   };
 
-  if (!canShowCart) {
-    if (typeof window !== "undefined") window.location.pathname = "/store";
-  }
-
-  const handleError = (message: string) => {
-    setError(message);
-    setWaiting(false);
-    setCheckout(true);
-  };
-
-  const handleSuccess = () => {
-    setWaiting(false);
-    setPayout(true);
-  };
-
-  const handlePayment = async (
-    data: Payment,
-    brand: string,
-    amount: number
-  ) => {
+  const handlePayment = (data: Payment, brand: string, amount: string) => {
     if (validateFields(data)) {
       setError(fieldsErrorMessage);
       return;
     }
+    handleRequest();
 
-    const [firstName, lastName] = data.name.split(" ");
-    const validate = formatValidate(data.cardValidate);
-    const service = new PaymentService({
-      card: {
-        brand,
-        ccv: data.cardCCV,
-        exp: validate,
-        name: data.name,
-        number: data.cardNumber,
-      },
-      customer: { firstName, lastName },
-      order: { amount, orderid: orderId },
-      provider: "cielo",
-    });
-
-    setCheckout(false);
-    setWaiting(true);
-
-    try {
-      const response = await service.execute();
-
-      if (response.getCode() !== 200) {
-        handleError(response.getMessage());
-        return;
-      }
-
-      handleSuccess();
-    } catch (err) {
-      handleError((err as Error).message);
-    }
+    fetcher.submit({ ...data, brand, amount }, { method: "post" });
   };
 
   useEffect(() => {
     localStorage.setItem("cart", JSON.stringify(cart));
-  }, [cart]);
+
+    if (response?.code !== 200 && response?.code !== undefined) {
+      handleError(response.message);
+    }
+
+    if (response?.code === 200 && response?.code !== undefined) {
+      handleSuccess(response.orderId);
+    }
+  }, [cart, response]);
 
   return (
     <Content>
       {canShowCart && <Cart cart={cart} clear={() => handleClear()} />}
       {!!waiting && <PaymentInProgress />}
-      {!!payout && <Paying orderId={orderId} />}
-      {!!error && <Error message={error} close={() => setError("")} />}
+      {!!payout && <Paying orderId={order} />}
+      {!!error && <ToastError message={error} close={() => setError("")} />}
       {!!checkout && (
-        <form
+        <fetcher.Form
           onSubmit={handleSubmit((data) =>
             handlePayment(
               data as Payment,
               brand.brand,
-              calculateCartTotal(cart)
+              calculateCartTotal(cart).toString()
             )
           )}
         >
@@ -163,7 +180,7 @@ export default function CheckoutPage() {
             </div>
           </CollapseContent>
           <input type='submit' className='mt-8 btn btn-warning' />
-        </form>
+        </fetcher.Form>
       )}
     </Content>
   );
